7feb26f5d8d636e33b7862fdcdb88957a6b0eb48
hanyonghao <hanyonghao@ibeiliao.com>
2018-03-20 15:18:13 +0800

    修复逻辑问题

--- src/customize/customize.js
+++ src/customize/customize.js
@@ -15,47 +15,12 @@ const Area = require('./module/area.js');
 	const $groupList = new GroupList('#group');
 	const $canvas = new Canvas('#canvas', '#container');
 
-		/* 尺寸 */
+
+	/* 尺寸 */
 	const $size = $('#size');
 	const $sizeWidth = $('#sizeWidth');
 	const $sizeHeight = $('#sizeHeight');
 	const $sizeInput = $sizeWidth.concat($sizeHeight);
-
-	$size.on('change', () => {
-		if ($size.data('checked', undefined, true)) {
-			$sizeInput.removeAttr('disabled');
-		} else {
-			$canvas.init();
-			$sizeInput.attr('disabled', 'disabled');
-		}
-	});
-
-	$sizeWidth.on('blur', () => {
-		let value = $sizeWidth.value();
-		if (/^\d+$/.test(value)) {
-			$sizeWidth.data('value', value);
-			$canvas.imgWidth = parseFloat(value);
-			$canvas.update();
-		} else {
-			$sizeWidth.value($sizeWidth.data('value'));
-		}
-	});
-
-	$sizeHeight.on('blur', () => {
-		let value = $sizeHeight.value();
-		if (/^\d+$/.test(value)) {
-			$sizeHeight.data('value', value);
-			$canvas.imgHeight = parseFloat(value);
-			$canvas.update();
-		} else {
-			$sizeHeight.value($sizeHeight.data('value'));
-		}
-	});
-
-	$canvas.ready(() => {
-		$sizeWidth.value($canvas.imgWidth).data('value', $canvas.imgWidth);
-		$sizeHeight.value($canvas.imgHeight).data('value', $canvas.imgHeight);
-	});
 
 
 	/* 索引页 */
@@ -137,12 +102,14 @@ const Area = require('./module/area.js');
 							name: '图片',
 							accept: 'image/*'
 						}).success((data) => {
-							loadData(options.data);
-							loadSetting(options.setting);
 
 							if (typeof cb === 'function') {
 								cb(data);
 							}
+
+							loadData(options.data);
+							loadSetting(options.setting);
+
 						});
 					},
 					cancel() {
@@ -150,12 +117,14 @@ const Area = require('./module/area.js');
 					}
 				});
 			} else {
-				loadData(options.data);
-				loadSetting(options.setting);
 
 				if (typeof cb === 'function') {
 					cb(options.image);
 				}
+
+				loadData(options.data);
+				loadSetting(options.setting);
+
 			}
 		});
 	}
@@ -238,6 +207,44 @@ const Area = require('./module/area.js');
 			.addClass('active')
 			.siblings()
 			.removeClass('active');
+	});
+
+
+	/* 尺寸 */
+	$size.on('change', () => {
+		if ($size.data('checked', undefined, true)) {
+			$sizeInput.removeAttr('disabled');
+		} else {
+			$canvas.init();
+			$sizeInput.attr('disabled', 'disabled');
+		}
+	});
+
+	$sizeWidth.on('blur', () => {
+		let value = $sizeWidth.value();
+		if (/^\d+$/.test(value)) {
+			$sizeWidth.data('value', value);
+			$canvas.imgWidth = parseFloat(value);
+			$canvas.update();
+		} else {
+			$sizeWidth.value($sizeWidth.data('value'));
+		}
+	});
+
+	$sizeHeight.on('blur', () => {
+		let value = $sizeHeight.value();
+		if (/^\d+$/.test(value)) {
+			$sizeHeight.data('value', value);
+			$canvas.imgHeight = parseFloat(value);
+			$canvas.update();
+		} else {
+			$sizeHeight.value($sizeHeight.data('value'));
+		}
+	});
+
+	$canvas.ready(() => {
+		$sizeWidth.value($canvas.imgWidth).data('value', $canvas.imgWidth);
+		$sizeHeight.value($canvas.imgHeight).data('value', $canvas.imgHeight);
 	});
 
 
